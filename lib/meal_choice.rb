78668ccbd90fccd2f3c7a1e364fe51a9c0982a38
Jon Noriega <noriega331@gmail.com>
2020-08-05 11:26:46 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -3,4 +3,4 @@ puts "What a nutritious meal!"
 return "A plate of #{protein} with #{veg1} and #{veg2}."
 end
 
-meal_choice("carrots","string beans")
+meal_choice("carrots","string beans","lentils")
